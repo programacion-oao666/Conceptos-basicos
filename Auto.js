a525f56c1cc8371e23a4d35e2ac427614e98562a
MMAGANADEBIA <mmaganadebia@gmail.com>
2019-02-08 15:31:29 -0600

    metodos, setters, etc.

--- Auto.js
+++ Auto.js
@@ -1,9 +1,42 @@
-class Auto{
-    constructor(marca, modelo, color, kilometraje)    {
-        this.marca = marca;
-        this.color = color;
-        this.kilometraje = kilometraje;
-        this.modelo = modelo;
-        this.estado = "Apagado";
+export default class Auto{
+    constructor(marca, modelo, color = "Blanco", kilometraje = "0")    {
+        this._marca = marca;
+        this._color = color;
+        this._kilometraje = kilometraje;
+        this._modelo = modelo;
+        this._estado = "Apagado";
+    }
+
+    get color(){
+        return this._color;
+    }
+
+    set color(color){
+        this._color = color.toUpperCase();
+    }
+
+    mostrarEstado(){
+        console.log(`El auto ${this._marca} ${this.color} del año ${this._modelo} tiene un kilometraje de ${this._kilometraje}kms y está ${this._estado}`);
+    }
+
+    encender(){
+        this._estado = "Encendido";
+        this.mostrarEstado();
+    }
+
+    apagar(){
+        this._estado = "Apagado";
+        this.mostrarEstado();
+    }
+
+    avanzar(velocidad, tiempo){
+        if(this._estado === "Apagado"){
+            console.log('El auto no puede avanzar porque esta apagado')
+        }
+        else {
+            let distancia = velocidad * tiempo;
+            this._kilometraje = this._kilometraje + distancia;
+            this.mostrarEstado();
+        }
     }
 }
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hola, mundo!");
+import Auto from "./Auto.js"
+
+let auto1 = new Auto("Ford", 2000, "Rojo", 100);
+let auto2 = new Auto("Nissan", 2018, "Blanco" );
+let auto3 = new Auto("VW", 2010);
+
+console.log(`Color ${auto1.color}`); //Lectura
+console.log(`color ${auto2.color}`); //Lectura
+auto1.color = "Amarillo"; //Escritura
+auto2.color = "Azul"; //Escritura
+
+auto1.encender();
+auto1.avanzar(50, 2);
+auto1.apagar();
+auto1.avanzar(20, 2);
+
+auto2.encender();
+auto3.encender();
