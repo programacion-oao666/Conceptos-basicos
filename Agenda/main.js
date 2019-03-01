3323f267ee67d45ca77bd01fcf21815b015e07a2
MMAGANADEBIA <mmaganadebia@gmail.com>
2019-03-01 15:51:51 -0600

    falta agregar numero de empleados y promedio de edad

--- Agenda/main.js
+++ Agenda/main.js
@@ -1,2 +1,19 @@
-import Empleado from "./Empleado.js"
+import Employee from "./Employee.js"
+import Agenda from "./Agenda.js"
 
+class Main{
+    constructor(){
+        let agenda = new Agenda(document.querySelector("#agenda"));
+        document.querySelector("#btnAdd").addEventListener("click", () =>{
+        let name = document.querySelector("#name").value;
+        let email = document.querySelector("#email").value;
+        let birthday = new Date(document.querySelector("#birthday").value);
+        
+        let employee = new Employee(name, email, birthday);
+
+        agenda.addEmployee(employee);
+        });
+    }
+}
+
+let m = new Main();
